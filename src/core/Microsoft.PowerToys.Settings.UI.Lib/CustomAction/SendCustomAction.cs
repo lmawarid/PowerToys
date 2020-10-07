d65ae1a6ab5325840f05ae7e62b4edba44008302
lmawarid <silverlobos@gmail.com>
2020-10-07 15:29:00 -0700

    Add CultureInfo for certain string operations

--- src/core/Microsoft.PowerToys.Settings.UI.Lib/CustomAction/SendCustomAction.cs
+++ src/core/Microsoft.PowerToys.Settings.UI.Lib/CustomAction/SendCustomAction.cs
@@ -25,7 +25,8 @@ namespace Microsoft.PowerToys.Settings.UI.Lib.CustomAction
             {
                 PropertyNamingPolicy = new CustomNamePolicy((propertyName) =>
                 {
-                    return propertyName.Equals("ModuleAction") ? moduleName : propertyName;
+                    // Using InvariantCulture comparison as this text is internal.
+                    return propertyName.Equals("ModuleAction", System.StringComparison.InvariantCulture) ? moduleName : propertyName;
                 }),
             };
 
